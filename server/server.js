9a50977de460577b4ab51c3bc428f1632421372f
suryacp23 <suryacp.com@gmail.com>
2025-07-03 15:09:29 +0530

    habit cron done

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./utils/database.js";
 import cors from "cors";
+import cron from "node-cron";
 dotenv.config();
 connectDB();
 
 import habitRoutes from "./routers/habitRoutes.js";
 import habitHistoryRoutes from "./routers/habitHistoryRoutes.js";
+import { updateHabitStreaks } from "./utils/updateHabitStreaks.js";
 
 const port = process.env.PORT || 5000;
 
@@ -17,6 +19,16 @@ app.use(cors());
 
 app.use("/api/habits", habitRoutes);
 app.use("/api/habit-history", habitHistoryRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal Server Error" });
+});
+
+cron.schedule("5 0 * * *", async () => {
+  await updateHabitStreaks();
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
