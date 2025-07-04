aed039eabe69b87ec00030311c2ac85f84308f4b
surya c <suryacp.com@gmail.com>
2025-07-04 17:48:37 +0530

    new  (#14)
    * habit cron done
    
    * google auth added
    
    * added login and routes
    
    * resolved the error
    
    * resolved some merge conflit errors
    
    ---------
    
    Co-authored-by: Ravi <146548361+Ravi1470@users.noreply.github.com>

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,30 +1,18 @@
-<<<<<<< HEAD
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-=======
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
->>>>>>> d35b3a13d2771b5a88aa32c4387dfc4e3cc7c1d4
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-<<<<<<< HEAD
+    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-  </StrictMode>,
-)
-=======
-    <QueryClientProvider client={queryClient}>
-      <App />
     </QueryClientProvider>
-  </StrictMode>
+  </StrictMode>,
 );
->>>>>>> d35b3a13d2771b5a88aa32c4387dfc4e3cc7c1d4
+
