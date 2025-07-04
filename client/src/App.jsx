32e91caa1fa18ab1747e3ad3582e53790c454a94
suryacp23 <suryacp.com@gmail.com>
2025-07-04 17:28:55 +0530

    resolved the error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Calender from "./components/Calender";
 import HabitCard from "./components/HabitCard";
 import Habitform from "./components/Habitform";
@@ -15,26 +14,6 @@ const App = () => {
       <Route path="/" element={<Home />} />
     </Routes>
    </div>
-=======
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Habit from "./pages/Habit";
-import HabitForm from "./pages/Habitform";
-
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={<Habit />}
-        />
-        <Route
-          path="/CreateForm"
-          element={<HabitForm />}
-        />
-      </Routes>
-    </Router>
->>>>>>> d35b3a13d2771b5a88aa32c4387dfc4e3cc7c1d4
   );
 };
 
