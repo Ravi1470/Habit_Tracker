a968e6b2099f5d91107f03f3af49d08cb8a35590
Sarathisaara <sarathisakthivel7@gmail.com>
2025-07-06 18:25:30 +0530

    counter feeature completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import GoogleSignIn from "./pages/GoogleSignIn";
 import Habit from "./pages/Habit";
 import Habitform from "./pages/Habitform";
+import CountTime from "./components/ui/Form/CountTime"
 
 const App = () => {
   return (
